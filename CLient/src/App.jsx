51b7961d13f8f069dd00d8a9ea6b3629985f482b
serious03black <146652203+Serious03black@users.noreply.github.com>
2025-05-24 11:39:54 +0530

    HOme page navbar update

--- CLient/src/App.jsx
+++ CLient/src/App.jsx
@@ -1,13 +1,19 @@
 import react,{ useState } from 'react'
-
+import VoiceListingTool from './pages/VoiceListingTool'
+import ListingPreview from './pages/ListingPreview'
+import HomePage from './pages/HomePage'
 
 function App() {
   
 
   return (
-    <>
-  
-    </>
+     <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create-listing" element={<VoiceListingTool />} />
+        <Route path="/preview" element={<ListingPreview />} />
+      </Routes>
+    </Router>
   )
 }
 
